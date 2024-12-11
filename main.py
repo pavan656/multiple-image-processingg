90c702c1f4636ca52eab86835eddbf3f8bbffc6e
pavan656 <69709176+pavan656@users.noreply.github.com>
2024-12-11 22:24:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import bg_remover,img_Compressor,about,contact
+import img_Compressor,about,contact
 
 hide_st_style = """
             <style>
@@ -27,16 +27,14 @@ class MultiApp:
         with st.sidebar:        
             app = option_menu(
                 menu_title="Main Menu",
-                options=['BG Remover','IMG Compressor','About','Contact Us'],
-                icons=['image','crop','book','envelope'],
+                options=['IMG Compressor','About','Contact Us'],
+                icons=['image','book','envelope'],
                 menu_icon='cast',
                 default_index=0
                 
                 )
 
         
-        if app == "BG Remover":
-            bg_remover.app()
         if app == "IMG Compressor":
             img_Compressor.app()
         if app == "About":
